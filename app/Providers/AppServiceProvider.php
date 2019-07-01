661b288ba8f3ebf1c3c3fd0bd3f9407680e03e46
satoru <sat1954fst@gmail.com>
2019-07-01 09:51:37 +0900

    .gitignore追記

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //追記heroku
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
